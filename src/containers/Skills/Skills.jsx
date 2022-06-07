361bb0d7280438acee6a4279bd9f47827208cda6
Kosi <iyiegbuk@gmail.com>
2022-06-07 15:25:37 +0100

    Edited Skills

--- src/containers/Skills/Skills.jsx
+++ src/containers/Skills/Skills.jsx
@@ -1,10 +1,7 @@
 import React from "react";
 import "./Skills.css";
-import { images } from "../../constants";
 import { FaHtml5, FaReact } from "react-icons/fa";
 import { SiCss3, SiJavascript, SiTypescript } from "react-icons/si";
-import { IoLogoSass } from "react-icons/io";
-import { BsFillBootstrapFill } from "react-icons/bs";
 import { ImGit } from "react-icons/im";
 import { GrNode, GrMysql } from "react-icons/gr";
 import { DiMongodb } from "react-icons/di";
@@ -72,38 +69,6 @@ const Skills = () => {
           <p>TYPESCRIPT</p>
         </div>
       </div>
-{/* 
-      <div className="skills-container">
-        <div className="british">
-          <img
-            src={images.british}
-            alt="british-icon"
-            className="british-icon"
-          />
-          <p>ANGIELSKI</p>
-          <p>C1/C2</p>
-        </div>
-
-        <div className="spanish">
-          <img
-            src={images.spanish}
-            alt="spanish-icon"
-            className="spanish-icon"
-          />
-          <p>HISZPANSKI</p>
-          <p>B1/B2</p>
-        </div>
-
-        <div className="cplus">
-          <img src={images.cplus} alt="cplus-icon" className="cplus-icon" />
-          <p>C++</p>
-        </div>
-
-        <div className="c">
-          <img src={images.clogo} alt="c-icon" className="c-icon" />
-          <p>C</p>
-        </div>
-      </div> */}
     </div>
   );
 };
